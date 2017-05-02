dba94ae9971d72b09719fdcfd72bddd558d0c3b9
Adam Orr <adamjorr@gmail.com>
2017-05-02 09:14:59 -0700

    it compiles! :confetti_ball:

--- tuple_print.cc
+++ tuple_print.cc
@@ -1,12 +1,5 @@
 #include "tuple_print.h"
 
-template<typename T0, typename...T>
-std::ostream& operator<<(std::ostream& os, const std::tuple<T0, T...> t){
-	os << '(' << std::get<0>(t); // << quote << std::get<0>(t) << quote;
-	TuplePrinter<1>::print(os,t);
-	return os << ')';
-}
-
 std::ostream& operator<<(std::ostream& os, const std::tuple<>){
 	return os << "()";
 }
--- tuple_print.h
+++ tuple_print.h
@@ -24,7 +24,11 @@ struct TuplePrinter{
 };
 
 template<typename T0, typename...T>
-std::ostream& operator<<(std::ostream& os, const std::tuple<T0, T...> t);
+std::ostream& operator<<(std::ostream& os, const std::tuple<T0, T...> t){
+	os << '(' << std::get<0>(t); // << quote << std::get<0>(t) << quote;
+	TuplePrinter<1>::print(os,t);
+	return os << ')';
+}
 
 std::ostream& operator<<(std::ostream& os, const std::tuple<>);
 
